c4ac9ee4f47994352e026a8cc168cfa8b1ca32f8
Al Biheiri <abiheiri@apple.com>
2020-09-15 11:11:55 -0700

    updating the ls flag

--- awsdns.py
+++ awsdns.py
@@ -24,11 +24,15 @@ def get_domains():
 
 
 # Method for dumping out the domain/zone list in a pretty format
-def list_domains():
+def list_domains(debug):
     get_domains()
 
-    domain_list = json.dumps(filtered_zone_list, indent=4, sort_keys=True)
-    print(domain_list)
+    if debug:
+        domain_list = json.dumps(filtered_zone_list, indent=4, sort_keys=True)
+        print(domain_list)
+    else:
+        for item in filtered_zone_list:
+            print (item.get('Name'))
 
 
 # Method for filtering and targeting a specific domain/zone, based on users args
@@ -161,6 +165,7 @@ def parse_args(args):
     parser = argparse.ArgumentParser(
         epilog='''
         Deleting a record requires exact values for: -rt -n -v --ttl. If any of these are not correct values, it will fail to delete.
+        You can use "get" arguments to get all the values before attempting to delete.
         '''
     )
 
@@ -168,24 +173,27 @@ def parse_args(args):
 
     group = parser.add_mutually_exclusive_group()
     group.add_argument("-ls", dest="ls", action="store_true", help="list domain you own")
+    
+
     group.add_argument("-g", dest="get", metavar='get', help="get all records from a domain")
     group.add_argument("-a", dest="add", metavar="add", help="add to domain")
     group.add_argument("-r", dest="replace", metavar="replace", help="replace record in a domain")
     group.add_argument("-d", dest="delete", metavar="delete", help="delete record a domain")
     group.add_argument("--author", dest="author", action="store_true", help="print developer info")
 
-
+    parser.add_argument("--debug", dest="debug", default=None, action="store_true", help="used in tandem with -ls to print more")
     parser.add_argument("-rt", dest="record", default=None, metavar="record type", choices=['A','AAAA','CNAME','TXT','MX','SRV','SOA','NS'], help="A,AAAA,CNAME,TXT,MX,SRV,SOA,NS")
     parser.add_argument("-n", dest="name", default=None, metavar="name", help="the dns record name")
     parser.add_argument("-v", dest="value", metavar="value", help="the value of the dns record (ie. ip address)")
     parser.add_argument('--ttl', type=int, default=3600, help='default is 3600 if this argument is not supplied')
+    
 
     args = parser.parse_args()
 
 
 
     if args.ls:
-        list_domains()
+        list_domains(args.debug)
 
     elif args.get:
         do_get(args.get, args.record, args.name)
